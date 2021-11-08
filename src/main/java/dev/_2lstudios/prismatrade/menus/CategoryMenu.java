00c54c2c70f24bbf398fd81c06c191543fa45470
LinsaFTW <25271111+linsaftw@users.noreply.github.com>
2021-11-08 10:54:29 -0300

    add example checkered items

--- src/main/java/dev/_2lstudios/prismatrade/menus/CategoryMenu.java
+++ src/main/java/dev/_2lstudios/prismatrade/menus/CategoryMenu.java
@@ -1,5 +1,9 @@
 package dev._2lstudios.prismatrade.menus;
 
+import org.bukkit.Material;
+
+import dev._2lstudios.hypermenus.api.Menu;
+import dev._2lstudios.hypermenus.api.MenuItem;
 import dev._2lstudios.hypermenus.api.NextItem;
 import dev._2lstudios.hypermenus.api.PageMenu;
 import dev._2lstudios.hypermenus.api.PreviousItem;
@@ -8,6 +12,11 @@ import dev._2lstudios.prismatrade.menus.items.NewOrderMenuItem;
 public class CategoryMenu extends PageMenu {
     public CategoryMenu(final int page) {
         super(page);
+
+        for (final int slot : Menu.CHECKERED_SLOTS) {
+            setItem(slot, new MenuItem().setType(Material.OAK_WOOD));
+        }
+
         setItem(45, new PreviousItem(this));
         setItem(49, new NewOrderMenuItem());
         setItem(53, new NextItem(this));
--- src/main/java/dev/_2lstudios/prismatrade/menus/InventoryMenu.java
+++ src/main/java/dev/_2lstudios/prismatrade/menus/InventoryMenu.java
@@ -1,5 +1,9 @@
 package dev._2lstudios.prismatrade.menus;
 
+import org.bukkit.Material;
+
+import dev._2lstudios.hypermenus.api.Menu;
+import dev._2lstudios.hypermenus.api.MenuItem;
 import dev._2lstudios.hypermenus.api.NextItem;
 import dev._2lstudios.hypermenus.api.PageMenu;
 import dev._2lstudios.hypermenus.api.PreviousItem;
@@ -8,6 +12,11 @@ import dev._2lstudios.prismatrade.menus.items.TakeAllItem;
 public class InventoryMenu extends PageMenu {
     public InventoryMenu(final int page) {
         super(page);
+
+        for (final int slot : Menu.CHECKERED_SLOTS) {
+            setItem(slot, new MenuItem().setType(Material.DIAMOND).setAmount(64));
+        }
+
         setItem(45, new PreviousItem(this));
         setItem(49, new TakeAllItem());
         setItem(53, new NextItem(this));
