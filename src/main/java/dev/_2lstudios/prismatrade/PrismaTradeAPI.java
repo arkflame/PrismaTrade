f1878992477c3765ac66f2eb30dcb776eef4efc3
LinsaFTW <linsaftw@users.noreply.github.com>
2022-04-28 12:29:38 -0300

    Insta-buy

--- src/main/java/dev/_2lstudios/prismatrade/PrismaTradeAPI.java
+++ src/main/java/dev/_2lstudios/prismatrade/PrismaTradeAPI.java
@@ -1,6 +1,8 @@
 package dev._2lstudios.prismatrade;
 
+import org.bukkit.Material;
 import org.bukkit.configuration.Configuration;
+import org.bukkit.entity.Player;
 
 import java.util.Collections;
 
@@ -39,5 +41,56 @@ public class PrismaTradeAPI {
     public BuyOrder[] getBuyOrders(int limit, int skip) {
         return buyOrderRepository.findMany(Collections.EMPTY_MAP,
                 FindOption.create().sort("id").sort("price", SortOrder.DESCENDANT).skip(skip).limit(limit));
+    }
+
+    public double getUnitaryPrice(int amount, double price) {
+        return amount / price;
+    }
+
+    public void buy(Player player, Material material, int amount, double price) {
+        // Insta-Buy - Start
+        for (SellOrder sellOrder : getSellOrders(10, 0)) {
+            final double unitaryPrice = getUnitaryPrice(amount, price);
+            final double sellPrice = sellOrder.getPrice();
+            int sellAmount = sellOrder.getAmount();
+            final int initialSellAmount = sellAmount;
+            final double sellUnitaryPrice = getUnitaryPrice(sellAmount, sellPrice);
+
+            if (unitaryPrice >= sellUnitaryPrice) {
+                if (amount >= sellAmount) {
+                    amount -= initialSellAmount;
+                    price = amount * unitaryPrice;
+
+                    sellOrder.delete();
+                } else {
+                    sellAmount -= amount;
+                    amount = 0;
+                    price -= initialSellAmount * sellUnitaryPrice;
+                    sellOrder.setPrice(sellUnitaryPrice * sellAmount);
+                    sellOrder.setAmount(sellAmount);
+                    sellOrder.save();
+                }
+
+                double toGive = initialSellAmount * unitaryPrice;
+
+                if (toGive > 0) {
+                    // TODO: Give money to seller
+                }
+
+                if (amount <= 0) {
+                    break;
+                }
+            }
+        }
+        // Insta-Buy - End
+
+        if (amount > 0) {
+            BuyOrder buyOrder = new BuyOrder();
+
+            buyOrder.setMaterial(material);
+            buyOrder.setAmount(amount);
+            buyOrder.setPrice(price);
+            buyOrder.save();
+        }
     }
 }
